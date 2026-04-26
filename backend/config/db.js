167ef3abf4be62d3c01b2791db07b6008aef31a6
RAFANOMEZANTSOA Anja Nofidina <anjarafanomezantsoa58@gmail.com>
2026-04-26 20:17:07 +0300

    fix: SSL Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,17 +4,25 @@ require('dotenv').config();
 let pool;
 
 if (process.env.MYSQL_URL) {
-  pool = mysql.createPool(process.env.MYSQL_URL);
+  pool = mysql.createPool({
+    uri: process.env.MYSQL_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
 } else {
   pool = mysql.createPool({
-    host:             process.env.DB_HOST,
-    port:             parseInt(process.env.DB_PORT) || 3306,
-    user:             process.env.DB_USER,
-    password:         process.env.DB_PASSWORD,
-    database:         process.env.DB_NAME,
+    host:     process.env.DB_HOST,
+    port:     parseInt(process.env.DB_PORT) || 3306,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit:  10,
-    queueLimit:       0
+    connectionLimit: 10,
+    queueLimit: 0
   });
 }
 
